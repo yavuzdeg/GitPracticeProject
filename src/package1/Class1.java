723db4dc1b924b65563f27de84c0bc5bdd4973f0
yavuzdeg <64091551+yavuzdeg@users.noreply.github.com>
2021-03-02 13:04:15 +0300

    Update Class1.java

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -11,7 +11,7 @@ public class Class1 {
 		System.out.println("Deneme4");
 		System.out.println("Deneme5");
 		System.out.println("Deneme7");
-
+                System.out.println("Deneme8");
 	}
 	
 	
